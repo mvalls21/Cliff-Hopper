9cb44bfb8ab9e01c6a54314adfb94101c9ff7572
matedavid <david.mate.lopez@gmail.com>
2023-05-02 12:59:08 +0200

    initial commit

---
+++ Assets/LevelGeneration.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+    }
+}
---
+++ Assets/MovementScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(1, -0.5f, 1) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(-1, 0.5f, -1) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+        }
+    }
+}
