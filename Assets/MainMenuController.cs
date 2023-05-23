496cced9aeb5349fde718d17a76ed8aa7bec3075
matedavid <david.mate.lopez@gmail.com>
2023-05-23 13:01:57 +0200

    simple main menu working

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartInfiniteMode()
+    {
+        LevelGeneration.InfiniteGeneration = true;
+        SceneManager.LoadScene("Scenes/Game");
+    }
+
+    public void StartEvaluationMode()
+    {
+        LevelGeneration.InfiniteGeneration = false;
+        SceneManager.LoadScene("Scenes/Game");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        
+        #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+        #endif
+    }
+}
