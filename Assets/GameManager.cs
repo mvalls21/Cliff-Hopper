7befbf83e271ac8d2177659039316945c33fc582
matedavid <david.mate.lopez@gmail.com>
2023-05-23 14:05:54 +0200

    coins and score on screen when playing

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,23 +1,61 @@
 using System;
+using Unity.VisualScripting.FullSerializer.Internal.Converters;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public Canvas playerDeadScreen;
 
+    public Text currentScoreText;
+
+    public Text currentCoinsText;
+
+    public Text coinIcon;
+
     public bool IsPlayerAlive { get; private set; } = true;
 
-    public int NumberCoins { get; private set; } = 0;
+    private int _numberCoins;
 
-    public int Score { get; private set; } = 0;
+    public int NumberCoins
+    {
+        get => _numberCoins;
+        private set
+        {
+            _numberCoins = value;
+            currentCoinsText.text = $"{value}";
+        }
+    } 
+
+    private int _score;
+
+    public int Score
+    {
+        get => _score;
+        private set
+        {
+            _score = value;
+            currentScoreText.text = $"{value}";
+        }
+    }
 
     public event EventHandler<int> ScoreChanged;
+
+    public void Start()
+    {
+        Score = 0;
+        NumberCoins = 0; // TODO: Should read from memory?
+    }
 
     public void PlayerDied()
     {
         IsPlayerAlive = false;
 
+        Destroy(currentScoreText);
+        Destroy(currentCoinsText);
+        Destroy(coinIcon);
+        
         var screen = playerDeadScreen.GetComponent<DeadScreenController>();
         screen.Show(Score);
     }
