9d780f1ae0e2a92f7fe3629a64edd43d10d2b225
matedavid <david.mate.lopez@gmail.com>
2023-05-27 23:33:49 +0200

    main menu music and game start sound

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -1,19 +1,38 @@
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    private AudioSource _audioSource;
+
+    private bool _gameStarting = false;
+
+    public GameObject backgroundMusicObject;
+
+    private AudioSource _backgroundSource;
+
+    public void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _backgroundSource = backgroundMusicObject.GetComponent<AudioSource>();
+    }
+
     public void StartInfiniteMode()
     {
+        if (_gameStarting) return;
+        
         LevelGeneration.InfiniteGeneration = true;
-        SceneManager.LoadScene("Scenes/Game");
+        StartCoroutine(StartGame());
     }
 
     public void StartEvaluationMode()
     {
+        if (_gameStarting) return;
+        
         LevelGeneration.InfiniteGeneration = false;
-        SceneManager.LoadScene("Scenes/Game");
+        StartCoroutine(StartGame());
     }
 
     public void Quit()
@@ -23,5 +42,17 @@ public class MainMenuController : MonoBehaviour
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #endif
+    }
+
+    private IEnumerator StartGame()
+    {
+        _gameStarting = true;
+        
+        _audioSource.Play();
+        _backgroundSource.Pause();
+        
+        yield return new WaitForSeconds(4.0f);
+
+        SceneManager.LoadScene("Scenes/Game");
     }
 }
